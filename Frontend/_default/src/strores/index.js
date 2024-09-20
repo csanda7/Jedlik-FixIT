e7e9d457f9e0b38b7ff0928908c30461915a9eae
Csanda Norbert <csandanorbert@gmail.com>
2024-09-20 10:29:47 +0200

    default

---
+++ Frontend/_default/src/strores/index.js
@@ -0,0 +1,8 @@
+import { defineStore } from "pinia";
+import Axios from '../services/dataservice';
+
+export const useXXXXXX = defineStore('ID',{
+    state: ()=>({ változok }),
+    getters:{},
+    actions:{}
+});
