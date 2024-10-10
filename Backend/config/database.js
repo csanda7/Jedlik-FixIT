dc50f14a03dbb4b9678bf6f6d8d828fe33ecfc3e
Admat1k <molnar.a2003@gmail.com>
2024-10-10 11:41:53 +0200

    cvnv

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -6,26 +6,11 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'jedlikfixit_teszt'
+  database: 'jedlikfixit_teszt',
+  charset: 'utf8mb4_hungarian_ci' // Ensure charset supports utf8mb4
 });
 
 connection.connect(function (error) {
-  if (error) {
-    console.error('Error connecting to the database:', error);
-    throw error;
-  } else {
-    console.log('Database connected');
-  }
-});
-
-const bugReport = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'jedlikfixit_teszt'
-});
-
-bugReport.connect(function (error) {
   if (error) {
     console.error('Error connecting to the database:', error);
     throw error;
