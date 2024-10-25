38e40502bd6e95ef52252272881e570bc697c429
Molnár Ádám <molnar.a2003@gmail.com>
2024-10-25 10:07:35 +0200

    bugfix

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
-const authRoutes = require('./routes/authRoutes'); // Adjusted to match your routes
+const { jsonParser } = require('./middlewares/bodyParser');
+
+const encoder = bodyParser.urlencoded({ extended: true });
+const authRoutes = require('./routes/authRoutes');
 const bugReportController = require('./controllers/bugReportController'); // Import bug report routes
 const hibakKiirRoutes = require('./routes/hibaKiirRoutes');
-const hibaFelvetelRoutes = require('./routes/hibaFelvetelRoutes'); // Import bug routes
+const hibaFelvetel = require('./routes/hibaFelvetelRoutes');  // Import bug routes
+
+
+
 
 const corsOptions = {
   origin: 'http://localhost:5173', // Change this to the correct URL of your Vue.js app
@@ -16,23 +21,27 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json()); // For parsing application/json
-app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+app.use(jsonParser);
 
 // Serve static files from the frontend (if necessary)
 const frontendPath = path.join(__dirname, '..', '..', 'frontend');
 app.use(express.static(frontendPath));
 
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+
 // Routes
-app.use('/api/login', authRoutes); // Ensure the route prefix is correct
+app.use('/api/login',authRoutes);
 app.use('/api/bugReport', bugReportController);
 app.use('/api/hibakKiir', hibakKiirRoutes);
-app.use('/api', hibaFelvetelRoutes);
+app.use('/api', hibaFelvetel);
 app.use('/uploads', express.static(path.join(__dirname, '../uploads'))); // Adjust to your actual path
+app.use('/api', bugReportController); // Make sure the route prefix is correct
 
 // Start the server
 const PORT = 4500;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
