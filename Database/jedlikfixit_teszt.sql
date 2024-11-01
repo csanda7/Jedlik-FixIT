933252871ec3c4a80dafb4ef587c6165158aae05
Molnár Ádám <molnar.a2003@gmail.com>
2024-11-01 12:01:01 +0100

    Az adatb lemaradt

--- Database/jedlikfixit_teszt.sql
+++ Database/jedlikfixit_teszt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Okt 31. 13:50
+-- Létrehozás ideje: 2024. Nov 01. 12:00
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -55,16 +55,16 @@ INSERT INTO `hibabejelentesek` (`ID`, `Title`, `Description`, `Reported_by`, `Lo
 (82, 'xcvycv', 'xcvyxcv', 'kovacs.bela', 'Másik terem 2', '2024-10-30 17:09:00', '2024-10-30 18:24:01', 2, 'Meghiúsult', 'Hardver', 'kovacs.bela', '2024-10-31 08:08:46'),
 (84, 'fadD', 'das', 'kovacs.bela', 'asd', '2024-10-30 17:09:19', '2024-10-30 18:25:32', 5, 'Kész', 'Egyéb', 'kovacs.bela', '2024-10-31 08:08:46'),
 (85, 'sadafvdv', 'xycvvyc', 'kovacs.bela', 'Másik terem 2', '2024-10-30 18:26:20', '2024-10-30 19:16:15', 3, 'Kész', 'Egyéb', 'kovacs.bela', '2024-10-31 08:08:46'),
-(86, '  vcv', 'cx', 'kovacs.bela', 'Másik terem 2', '2024-10-30 18:26:25', '2024-10-31 12:40:12', 3, 'Folyamatban', 'Szoftver', 'kovacs.bela', '2024-10-31 08:08:46'),
+(86, '  vcv', 'cx', 'kovacs.bela', 'Másik terem 2', '2024-10-30 18:26:25', '2024-11-01 09:44:30', 3, 'Beszerzésre vár', 'Szoftver', 'kovacs.bela', '2024-10-31 08:08:46'),
 (87, 'sdafsd', 'asdf', 'kovacs.bela', 'Másik terem 2', '2024-10-30 18:26:30', '2024-10-31 12:40:40', 1, 'Kész', 'Hardver', 'kovacs.bela', '2024-10-31 08:08:46'),
 (88, 'ícxvy', 'cyv', 'kovacs.bela', 'Másik terem 2', '2024-10-30 18:26:36', '2024-10-31 08:25:43', 5, 'Kész', 'Hardver', 'kovacs.bela', '2024-10-31 08:08:46'),
 (89, 'jlk', 'ghd', 'kovacs.bela', 'yxc', '2024-10-30 18:26:42', '2024-10-30 18:26:42', 3, 'Bejelentve', 'Hardver', NULL, '2024-10-31 08:08:46'),
 (90, 'fdyv', 'yxcv', 'kovacs.bela', 'Másik terem 2', '2024-10-31 08:44:50', '2024-10-31 10:05:34', 2, 'Kész', 'Szoftver', 'kovacs.bela', '2024-10-31 08:44:50'),
-(91, 'sda', 'Írok két mondatot. Kettő mondat, a Gergő egy buzi.', 'kovacs.bela', 'Másik terem 2', '2024-10-31 08:54:54', '2024-10-31 08:54:54', 2, 'Bejelentve', 'Hardver', NULL, '2024-10-31 08:54:54'),
+(91, 'sda', 'Írok két mondatot. Kettő mondat, a Gergő egy buzi.', 'kovacs.bela', 'Másik terem 2', '2024-10-31 08:54:54', '2024-11-01 09:55:09', 2, 'Folyamatban', 'Hardver', 'kovacs.bela', '2024-10-31 08:54:54'),
 (92, 'scv', 'mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm', 'kovacs.bela', 'Másik terem 2', '2024-10-31 08:58:40', '2024-10-31 10:08:15', 3, 'Kész', 'Hardver', 'kovacs.bela', '2024-10-31 08:58:40'),
 (93, 'ydsfy', 'fdsvd', 'kovacs.bela', 'Másik terem 2', '2024-10-31 09:27:15', '2024-10-31 09:27:15', 2, 'Bejelentve', 'Szoftver', NULL, '2024-10-31 08:26:00'),
 (94, 'gj', 'gfhfghf', 'kovacs.bela', 'Első terem', '2024-10-31 10:12:04', '2024-10-31 10:12:04', 4, 'Bejelentve', 'Szoftver', NULL, '2024-10-31 10:11:00'),
-(95, 'fdxvc', 'xcv', 'kovacs.bela', 'Másik terem 2', '2024-10-31 10:21:10', '2024-10-31 10:21:10', 0, 'Bejelentve', 'Hardver', NULL, '2024-10-31 10:21:00');
+(95, 'fdxvc', 'xcv', 'kovacs.bela', 'Másik terem 2', '2024-10-31 10:21:10', '2024-11-01 09:56:51', 0, 'Folyamatban', 'Hardver', 'kovacs.bela', '2024-10-31 10:21:00');
 
 -- --------------------------------------------------------
 
@@ -102,27 +102,36 @@ CREATE TABLE `log` (
   `ID` int(11) NOT NULL,
   `LStatus` varchar(255) CHARACTER SET utf16 COLLATE utf16_hungarian_ci NOT NULL,
   `Komment` varchar(300) NOT NULL,
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `modosito` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `log`
 --
 
-INSERT INTO `log` (`ID`, `LStatus`, `Komment`, `updated_at`) VALUES
-(90, 'Beszerzésre vár', '', '2024-10-31 12:45:15'),
-(92, 'Folyamatban', '', '2024-10-31 12:45:15'),
-(94, 'Bejelentve', '', '2024-10-31 12:45:15'),
-(95, 'Bejelentve', '', '2024-10-31 12:45:15'),
-(92, 'Kész', '', '2024-10-31 12:45:15'),
-(88, 'Kész', '', '2024-10-31 12:45:15'),
-(81, 'Kész', '', '2024-10-31 12:45:15'),
-(90, 'Folyamatban', '', '2024-10-31 12:45:15'),
-(90, 'Kész', '', '2024-10-31 12:45:15'),
-(86, 'Folyamatban', '', '2024-10-31 12:45:15'),
-(87, 'Folyamatban', '', '2024-10-31 12:45:15'),
-(87, 'Beszerzésre vár', '', '2024-10-31 12:45:15'),
-(87, 'Kész', '', '2024-10-31 12:45:15');
+INSERT INTO `log` (`ID`, `LStatus`, `Komment`, `updated_at`, `modosito`) VALUES
+(90, 'Beszerzésre vár', '', '2024-10-31 12:45:15', NULL),
+(92, 'Folyamatban', '', '2024-10-31 12:45:15', NULL),
+(94, 'Bejelentve', '', '2024-10-31 12:45:15', NULL),
+(95, 'Bejelentve', '', '2024-10-31 12:45:15', NULL),
+(92, 'Kész', '', '2024-10-31 12:45:15', NULL),
+(88, 'Kész', '', '2024-10-31 12:45:15', NULL),
+(81, 'Kész', '', '2024-10-31 12:45:15', NULL),
+(90, 'Folyamatban', '', '2024-10-31 12:45:15', NULL),
+(90, 'Kész', '', '2024-10-31 12:45:15', NULL),
+(86, 'Folyamatban', '', '2024-10-31 12:45:15', NULL),
+(87, 'Folyamatban', '', '2024-10-31 12:45:15', NULL),
+(87, 'Beszerzésre vár', '', '2024-10-31 12:45:15', NULL),
+(87, 'Kész', '', '2024-10-31 12:45:15', NULL),
+(86, '', 'asd', '2024-11-01 09:27:25', 'kovacs.bela'),
+(86, 'Beszerzésre vár', 'asd', '2024-11-01 09:44:04', 'kovacs.bela'),
+(86, '', 'gdfsg', '2024-11-01 09:44:30', 'kovacs.bela'),
+(91, 'Folyamatban', 'asd', '2024-11-01 09:55:09', 'kovacs.bela'),
+(95, 'Folyamatban', '', '2024-11-01 09:55:48', 'kovacs.bela'),
+(95, '', '', '2024-11-01 09:56:51', 'kovacs.bela'),
+(95, 'Beszerzésre vár', '', '2024-11-01 09:59:28', 'kovacs.bela'),
+(95, 'Folyamatban', '', '2024-11-01 10:55:29', 'kovacs.bela');
 
 --
 -- Indexek a kiírt táblákhoz
